2deabd2f9b6d3a7054e7903e59134e9efd6f51a3
cereza-rainier <cmyocyte@gmail.com>
2026-03-12 16:29:45 -0400

    Updates

--- _next/static/3ao9FcTFYdYFKMfOEnnGE/_buildManifest.js
+++ _next/static/3ao9FcTFYdYFKMfOEnnGE/_buildManifest.js
@@ -1,12 +1,7 @@
 self.__BUILD_MANIFEST = {
   "__rewrites": {
     "afterFiles": [],
-    "beforeFiles": [
-      {
-        "source": "/paraflux//_next/:path+",
-        "destination": "/paraflux/_next/:path+"
-      }
-    ],
+    "beforeFiles": [],
     "fallback": []
   },
   "sortedPages": [
